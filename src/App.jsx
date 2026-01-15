bee8475f51683ea5289f24eb72739c687399e793
annu933 <kashyapannu933@gmail.com>
2026-01-15 16:41:19 +0530

    add auth context and protected routes .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import { useAuth } from "./context/AuthContext";
+import Upload from "./pages/Upload";
 
 function App() {
+
+  const ProtectedRoute = ({ children }) => {
+    const { token } = useAuth();
+    console.log("token", token)
+    return token ? children : <Navigate to="/login" replace />
+  }
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div>App Initialized</div>} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<div>App Initialized</div>} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/upload"
+        element={
+          <ProtectedRoute>
+            <Upload />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
 
