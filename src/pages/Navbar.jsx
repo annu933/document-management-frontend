d5e5e43ae011034b8229bd3116e8954b8b1fba15
annu933 <kashyapannu933@gmail.com>
2026-01-16 23:20:17 +0530

    navbar created

---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,31 @@
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+export default function Navbar() {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("/");
+    };
+    const isLogin = localStorage.getItem("token");
+    console.log("isLogin", isLogin)
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                {/* Left */}
+                <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                    Allsoft Assignment
+                </Typography>
+
+                {/* Right */}
+                {isLogin === "null" && (
+                    <Button color="inherit" onClick={handleLogout}>
+                        Logout
+                    </Button>
+                )}
+
+            </Toolbar>
+        </AppBar>
+    );
+}
